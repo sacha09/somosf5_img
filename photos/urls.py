db91f339a6e7f0f4e03d8f2fa273ee164aedc67b
Sacha <sacha@routchenko.com>
2023-10-23 14:55:33 +0400

    detail photo view

--- photos/urls.py
+++ photos/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
     PhotoListView,
+    PhotoDetailView,
 )
 
 app_name = 'photos'
 
 urlpatterns = [
     path('', PhotoListView.as_view(), name='photo_list'),
+    path('<int:pk>/', PhotoDetailView.as_view(), name='photo_detail'),
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Photo
 
 class PhotoListView(ListView):
     model = Photo
     template_name = 'photos/photo_list.html'
     context_object_name = 'photos'
+
+class PhotoDetailView(DetailView):
+    model = Photo
+    template_name = 'photo/photo_detail.html'
+    context_object_name = 'photo'
